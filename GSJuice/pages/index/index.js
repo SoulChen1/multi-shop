92ebcfbfcd51b7a51d69005c07680b6ba2f7b301
JackenSoul <37260532+SoulChen1@users.noreply.github.com>
2019-10-30 20:24:39 +0800

    取餐页面和个人信息页面

--- GSJuice/pages/index/index.js
+++ GSJuice/pages/index/index.js
@@ -4,50 +4,15 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     type: 'bySelf',
     item: 'item1',
     toView: '',
     toViewMenu: '',
-    heightArr: []
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    heightArr: [],
+    containerH: 0
   },
   onLoad: function() {
-    //WU
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    
   },
   onReady: function() {
     /*获取商品栏标题位置*/
@@ -55,9 +20,16 @@ Page({
     let queryTop = wx.createSelectorQuery();
     let heightArr = [];
     let topHeight = 0;
+    let containerH = 0;
     //顶部高度
     queryTop.select('.top').boundingClientRect((res) => {
       topHeight = res.height;
+    }).exec();
+    queryTop.select('.productList').boundingClientRect((res) => {
+      containerH = res.height;
+      this.setData({
+        containerH: containerH
+      });
     }).exec();
     //商品标题高度
     queryItem.selectAll('.item-title').boundingClientRect((react) => {
@@ -92,15 +64,13 @@ Page({
   scrollRight: function(e) {
     let scrollArr = this.data.heightArr;
     let scrollTop = e.detail.scrollTop - scrollArr[0];
-    // console.log(scrollTop);
+    let containerH = Math.round(e.detail.scrollHeight - this.data.containerH - scrollArr[0]);
     
-    
-    if (scrollTop >= scrollArr[scrollArr.length - 1]) {
+    if (scrollTop >= scrollArr[scrollArr.length - 1] || scrollTop == containerH) {
       return
     } else {
       for (let i = 0; i < scrollArr.length; i++) {
         if (scrollTop >= 0 && scrollTop < scrollArr[1] - scrollArr[0]) {
-          console.log(i);
           this.setData({
             toViewMenu: 'menuitem1',
             item: 'item1'
