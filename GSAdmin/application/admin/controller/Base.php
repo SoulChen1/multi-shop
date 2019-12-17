6c2c4e60841286cf80ac4c607a67a7bc6c872573
JackenSoul <37260532+SoulChen1@users.noreply.github.com>
2019-12-17 17:58:31 +0800

    菜单修改 系统开发日志编写

--- GSAdmin/application/admin/controller/Base.php
+++ GSAdmin/application/admin/controller/Base.php
@@ -6,18 +6,22 @@ use think\Controller;
 
 class Base extends Controller
 {
-    /*
+    /**
      * 问题：
      * 1.超级管理员登录问题(目前为店铺登录)
      * */
     //控制器初始化方法
    public function initialize()
    {
+       //判断session中是否有store.id
        if (!session('?store.id')) {
            $this->redirect('admin/login/storeLogin');
        }
    }
 
+   /**
+    * 登出后清空session并重定向到登录页面
+    **/
    public function loginOut(){
        session('store', null);
        $this->redirect('admin/login/storeLogin');
